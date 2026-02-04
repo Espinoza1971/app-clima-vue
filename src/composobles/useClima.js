29caa8d947eb60c9f3d8a53ec579cecb728498f5
Guillermo Espinoza <gespinoza@pami.org.ar>
2026-02-04 11:01:52 -0300

    fix

--- src/composobles/useClima.js
+++ src/composobles/useClima.js
@@ -17,7 +17,7 @@ export default function useClima() {
     error.value = ''
 
     try {
-        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+        const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
         const {data} = await axios(url)
         const { lat, lon } = data[0]
 
@@ -43,3 +43,4 @@ export default function useClima() {
         obtenerClima, clima, mostrarClima, formatearTemperatura, cargando, error
     }
 }
+
