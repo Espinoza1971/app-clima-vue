0750f9b37012c07b585ad81d151dd40bd87f20a0
Guillermo Espinoza <gespinoza@pami.org.ar>
2026-02-04 09:38:43 -0300

    prmier commit

---
+++ src/components/Clima.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import useClima from '../composobles/useClima';
+
+const { formatearTemperatura } = useClima()
+
+defineProps({
+    clima: {
+        type:Object,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="resultado">
+        <h2>Clima de: {{clima.name}} </h2>
+        <p class="actual">&deg;C {{ formatearTemperatura(clima.main.temp) }}</p>
+        <div class="temperaturas">
+            <p>Min: <span>&deg;C {{ formatearTemperatura(clima.main.temp_min) }}</span></p>
+            <p>Máx: <span>&deg;C {{ formatearTemperatura(clima.main.temp_max) }}</span></p>
+        </div>
+    </div>
+</template>
+
+
